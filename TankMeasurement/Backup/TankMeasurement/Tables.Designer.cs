c9311fabb7a12c2814351d2aa4b14479bee98ed3
ahmadfarisfs <ahmad.faris@formulatrix.com>
2018-02-09 09:50:18 +0700

    Migrate to VS 2017

---
+++ TankMeasurement/Backup/TankMeasurement/Tables.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace TankMeasurement
+{
+    partial class Tables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SplitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.table54 = new System.Windows.Forms.DataGridView();
+            this.Panel2 = new System.Windows.Forms.Panel();
+            this.File54 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tableTangki = new System.Windows.Forms.DataGridView();
+            this.Panel3 = new System.Windows.Forms.Panel();
+            this.FileTangki = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.tableLevel = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.FileLevel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SplitContainer1.Panel1.SuspendLayout();
+            this.SplitContainer1.Panel2.SuspendLayout();
+            this.SplitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table54)).BeginInit();
+            this.Panel2.SuspendLayout();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableTangki)).BeginInit();
+            this.Panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableLevel)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer1
+            // 
+            this.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer1.Name = "SplitContainer1";
+            // 
+            // SplitContainer1.Panel1
+            // 
+            this.SplitContainer1.Panel1.Controls.Add(this.table54);
+            this.SplitContainer1.Panel1.Controls.Add(this.Panel2);
+            // 
+            // SplitContainer1.Panel2
+            // 
+            this.SplitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.SplitContainer1.Size = new System.Drawing.Size(1236, 469);
+            this.SplitContainer1.SplitterDistance = 524;
+            this.SplitContainer1.TabIndex = 3;
+            // 
+            // table54
+            // 
+            this.table54.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table54.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table54.Location = new System.Drawing.Point(0, 26);
+            this.table54.Name = "table54";
+            this.table54.Size = new System.Drawing.Size(524, 443);
+            this.table54.TabIndex = 3;
+            // 
+            // Panel2
+            // 
+            this.Panel2.BackColor = System.Drawing.Color.Silver;
+            this.Panel2.Controls.Add(this.File54);
+            this.Panel2.Controls.Add(this.Label1);
+            this.Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel2.Location = new System.Drawing.Point(0, 0);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(524, 26);
+            this.Panel2.TabIndex = 2;
+            // 
+            // File54
+            // 
+            this.File54.AutoSize = true;
+            this.File54.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.File54.Location = new System.Drawing.Point(85, 7);
+            this.File54.Name = "File54";
+            this.File54.Size = new System.Drawing.Size(41, 15);
+            this.File54.TabIndex = 1;
+            this.File54.Text = "None";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(12, 7);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(73, 16);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Tabel 54:";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tableTangki);
+            this.splitContainer2.Panel1.Controls.Add(this.Panel3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tableLevel);
+            this.splitContainer2.Panel2.Controls.Add(this.panel4);
+            this.splitContainer2.Size = new System.Drawing.Size(708, 469);
+            this.splitContainer2.SplitterDistance = 411;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tableTangki
+            // 
+            this.tableTangki.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableTangki.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableTangki.Location = new System.Drawing.Point(0, 26);
+            this.tableTangki.Name = "tableTangki";
+            this.tableTangki.Size = new System.Drawing.Size(411, 443);
+            this.tableTangki.TabIndex = 3;
+            // 
+            // Panel3
+            // 
+            this.Panel3.BackColor = System.Drawing.Color.Silver;
+            this.Panel3.Controls.Add(this.FileTangki);
+            this.Panel3.Controls.Add(this.Label2);
+            this.Panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel3.Location = new System.Drawing.Point(0, 0);
+            this.Panel3.Name = "Panel3";
+            this.Panel3.Size = new System.Drawing.Size(411, 26);
+            this.Panel3.TabIndex = 2;
+            // 
+            // FileTangki
+            // 
+            this.FileTangki.AutoSize = true;
+            this.FileTangki.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileTangki.Location = new System.Drawing.Point(139, 7);
+            this.FileTangki.Name = "FileTangki";
+            this.FileTangki.Size = new System.Drawing.Size(41, 15);
+            this.FileTangki.TabIndex = 1;
+            this.FileTangki.Text = "None";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(28, 7);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(105, 16);
+            this.Label2.TabIndex = 0;
+            this.Label2.Text = "Tabel Tangki:";
+            // 
+            // tableLevel
+            // 
+            this.tableLevel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLevel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLevel.Location = new System.Drawing.Point(0, 26);
+            this.tableLevel.Name = "tableLevel";
+            this.tableLevel.Size = new System.Drawing.Size(293, 443);
+            this.tableLevel.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Silver;
+            this.panel4.Controls.Add(this.FileLevel);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(293, 26);
+            this.panel4.TabIndex = 4;
+            // 
+            // FileLevel
+            // 
+            this.FileLevel.AutoSize = true;
+            this.FileLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FileLevel.Location = new System.Drawing.Point(139, 7);
+            this.FileLevel.Name = "FileLevel";
+            this.FileLevel.Size = new System.Drawing.Size(41, 15);
+            this.FileLevel.TabIndex = 1;
+            this.FileLevel.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tabel Level:";
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.Button3);
+            this.Panel1.Controls.Add(this.button4);
+            this.Panel1.Controls.Add(this.Button2);
+            this.Panel1.Controls.Add(this.Button1);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panel1.Location = new System.Drawing.Point(0, 469);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(1236, 35);
+            this.Panel1.TabIndex = 2;
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(1149, 6);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(75, 23);
+            this.Button3.TabIndex = 0;
+            this.Button3.Text = "Save";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1068, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Load Level";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(987, 6);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 0;
+            this.Button2.Text = "Load Tangki";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(906, 6);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Load 54";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Tables
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 504);
+            this.Controls.Add(this.SplitContainer1);
+            this.Controls.Add(this.Panel1);
+            this.Name = "Tables";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Tables";
+            this.SplitContainer1.Panel1.ResumeLayout(false);
+            this.SplitContainer1.Panel2.ResumeLayout(false);
+            this.SplitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.table54)).EndInit();
+            this.Panel2.ResumeLayout(false);
+            this.Panel2.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tableTangki)).EndInit();
+            this.Panel3.ResumeLayout(false);
+            this.Panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tableLevel)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.SplitContainer SplitContainer1;
+        internal System.Windows.Forms.DataGridView table54;
+        internal System.Windows.Forms.Panel Panel2;
+        internal System.Windows.Forms.Label File54;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.DataGridView tableTangki;
+        internal System.Windows.Forms.Panel Panel3;
+        internal System.Windows.Forms.Label FileTangki;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        internal System.Windows.Forms.DataGridView tableLevel;
+        internal System.Windows.Forms.Panel panel4;
+        internal System.Windows.Forms.Label FileLevel;
+        internal System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.Button button4;
+
+    }
+}
